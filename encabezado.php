344dfcec80f7bb3218ba48b33a21f979139ba260
ArielOsorio <a21110182@ceti.mx>
2023-11-22 19:28:08 -0600

    Se quitan excesos

--- encabezado.php
+++ encabezado.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrito de compras con PHP y MySQL - By Parzibyte</title>
+    <title>Carrito de compras</title>
     <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
 </head>
@@ -13,14 +13,6 @@
 
     <nav class="navbar is-warning" role="navigation" aria-label="main navigation">
         <div class="navbar-brand">
-            <a class="navbar-item" href="https://parzibyte.me/blog">
-                <img alt="" src="https://raw.githubusercontent.com/parzibyte/ejemplo-mern/master/src/img/parzibyte_logo.png" style="max-height: 80px" />
-            </a>
-            <button class="navbar-burger is-warning button" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </button>
         </div>
         <div class="navbar-menu">
             <div class="navbar-start">
@@ -38,13 +30,6 @@
                                                     printf("(%d)", $conteo);
                                                 }
                                                 ?>&nbsp;<i class="fa fa-shopping-cart"></i></strong>
-                        </a>
-                    </div>
-                </div>
-                <div class="navbar-item">
-                    <div class="buttons">
-                        <a target="_blank" rel="noreferrer" href="https://parzibyte.me/l/fW8zGd" class="button is-primary">
-                            <strong>Soporte y ayuda</strong>
                         </a>
                     </div>
                 </div>
